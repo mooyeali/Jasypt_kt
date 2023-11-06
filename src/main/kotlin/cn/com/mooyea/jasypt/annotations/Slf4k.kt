206f4e4363efc8945543be810c6b4df77d8b41c9
lideyang <lidy@skyvis.com.cn>
2023-11-06 23:36:21 +0800

    feat(dev): 功能开发
    1.gui开发(主窗口及弹出层窗口)
    2.加解密功能开发
    3.H2 数据库集成
    4.加解密数据存储
    5.历史记录功能开发

---
+++ src/main/kotlin/cn/com/mooyea/jasypt/annotations/Slf4k.kt
@@ -0,0 +1,45 @@
+package cn.com.mooyea.jasypt.annotations
+
+
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+
+/**
+ * <h1>Slf4k<h1>
+ * <p>Copyright (C), 星期五,03,11月,2023</p>
+ * <br/>
+ * <hr>
+ * <h3>File Info:</h3>
+ * <p>FileName: Slf4k</p>
+ * <p>Author:   mooye</p>
+ * <p>Work_Email： lidy@skyvis.com.cn</p>
+ * <p>E-mail： mooyeali@yeah.net</p>
+ * <p>Date:     2023/11/3</p>
+ * <p>Description: 基于 lombok 的 Slf4j 注解适配 Kotlin 日志注解</p>
+ * <hr>
+ * <h3>History:</h3>
+ * <hr>
+ * <table>
+ *  <thead>
+ *  <tr><td style='width:100px;' center>Author</td><td style='width:200px;' center>Time</td><td style='width:100px;' center>Version_Number</td><td style='width:100px;' center>Description</td></tr>
+ *  </thead>
+ *  <tbody>
+ *    <tr><td style='width:100px;' center>mooye</td><td style='width:200px;' center>15:09 2023/11/3</td><td style='width:100px;' center>v_1.0.0</td><td style='width:100px;' center>创建</td></tr>
+ *  </tbody>
+ * </table>
+ * <hr>
+ * <br/>
+ *@author mooye
+ */
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Slf4k {
+
+    companion object {
+        val <reified T> T.log: Logger
+            inline get() = LogManager.getLogger(T::class.java)
+
+    }
+}
--- src/main/kotlin/module-info.java
+++ src/main/kotlin/module-info.java
@@ -7,8 +7,14 @@ module cn.com.mooyea.jasypt {
     requires org.kordamp.bootstrapfx.core;
     requires lombok;
     requires jasypt;
+    requires java.xml.crypto;
+    requires org.apache.logging.log4j;
+    requires java.sql;
 
     opens cn.com.mooyea.jasypt to javafx.fxml;
     opens cn.com.mooyea.jasypt.controller to javafx.fxml;
+    opens cn.com.mooyea.jasypt.handler to javafx.fxml;
+    opens cn.com.mooyea.jasypt.annotations to javafx.fxml;
+    opens cn.com.mooyea.jasypt.entity to javafx.base;
     exports cn.com.mooyea.jasypt;
 }
